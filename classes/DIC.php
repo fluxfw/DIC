c5cdc9d381e8e2fc8276ce1f37abf7637d7a16a5
Florian <fw@studer-raimann.ch>
2018-06-22 14:48:03 +0200

    UIServices

--- classes/DIC.php
+++ classes/DIC.php
@@ -2,34 +2,60 @@
 
 namespace srag;
 
+use ilAccess;
+use ilAppEventHandler;
+use ilCtrl;
+use ilDB;
+use ILIAS\DI\Container;
+use ILIAS\DI\HTTPServices;
+use ILIAS\DI\LoggingServices;
+use ILIAS\DI\UIServices;
+use ILIAS\Filesystem\Filesystems;
+use ILIAS\FileUpload\FileUpload;
+use ilLanguage;
+use ilObjUser;
+use ilPlugin;
+use ilRbacAdmin;
+use ilRbacReview;
+use ilRbacSystem;
+use ilSetting;
+use ilTabsGUI;
+use ilTemplate;
+use ilToolbarGUI;
+use ilTree;
+use ReflectionClass;
+
 /**
  * Use all $DIC properties dynamic in your class
  *
  * For autocomplete:
  *
- * @property \ILIAS\DI\Container           $DIC
- * @property \ILIAS\Filesystem\Filesystems $filesystem
- * @property \ilAccess                     $ilAccess
- * @property \ilAppEventHandler            $ilAppEventHandler
- * @property \ilCtrl                       $ilCtrl
- * @property \ilDB                         $ilDB
- * @property \ilSetting                    $ilSetting
- * @property \ilTabsGUI                    $ilTabs
- * @property \ilToolbarGUI                 $ilToolbar
- * @property \ilObjUser                    $ilUser
- * @property \ilLanguage                   $lng
- * @property \ilPlugin                     $pl
- * @property \ilRbacAdmin                  $rbacadmin
- * @property \ilRbacReview                 $rbacreview
- * @property \ilRbacSystem                 $rbacsystem
- * @property \ilTemplate                   $tpl
- * @property \ilTree                       $tree
- * @property \ILIAS\FileUpload\FileUpload  $upload
+ * @property Container         $DIC
+ * @property Filesystems       $filesystem
+ * @property HTTPServices      $http
+ * @property ilAccess          $ilAccess
+ * @property ilAppEventHandler $ilAppEventHandler
+ * @property ilCtrl            $ilCtrl
+ * @property ilDB              $ilDB
+ * @property ilSetting         $ilSetting
+ * @property ilTabsGUI         $ilTabs
+ * @property ilToolbarGUI      $ilToolbar
+ * @property ilObjUser         $ilUser
+ * @property ilLanguage        $lng
+ * @property LoggingServices   $logger
+ * @property ilPlugin          $pl
+ * @property ilRbacAdmin       $rbacadmin
+ * @property ilRbacReview      $rbacreview
+ * @property ilRbacSystem      $rbacsystem
+ * @property ilTemplate        $tpl
+ * @property ilTree            $tree
+ * @property UIServices        $ui
+ * @property FileUpload        $upload
  */
 trait DIC {
 
 	/**
-	 * @return \ilPlugin
+	 * @return ilPlugin
 	 */
 	private static function getPluginInstance() {
 		static $pl_caches = [];
@@ -37,7 +63,7 @@ trait DIC {
 		$current_class = self::class;
 
 		if (!isset($pl_caches[$current_class])) {
-			$reflect = new \ReflectionClass($current_class);
+			$reflect = new ReflectionClass($current_class);
 
 			$comment = $reflect->getDocComment();
 
@@ -71,7 +97,7 @@ trait DIC {
 		global $DIC;
 
 		if ($DIC === NULL) {
-			// Older ILIAS versions support
+			// Some older ILIAS versions limited support
 			$DIC = $GLOBALS;
 		}
 
@@ -79,8 +105,14 @@ trait DIC {
 			case "DIC":
 				return $DIC;
 
+			case "logger":
+				return $DIC->logger();
+
 			case "pl":
 				return self::getPluginInstance();
+
+			case "ui":
+				return $DIC->ui();
 
 			default:
 				if ($DIC->offsetExists($name)) {
